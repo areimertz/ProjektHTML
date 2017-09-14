efaacea83985ac24e37831726e42d31256802bd0
Jorgensendavid <david.jorgensen95@outlook.com>
2017-09-14 14:48:32 +0200

    commit a bit

--- projekt.js
+++ projekt.js
@@ -9,7 +9,7 @@ function validate(element, regex) {
 
     var isValid = regex.test(value);
 
-    element.style.backgroundColor = isValid ? "green" : "red";
+    element.style.backgroundColor = isValid ? "white" : "red";
 }
 
 usernameElement.addEventListener("keyup", function(){
@@ -37,10 +37,13 @@ function check(){
     var namnval = document.getElementById("username").value;
     var numberval = document.getElementById("number").value;
     var meddelandeval = document.getElementById("meddelande").value;
-    if(namnval == null || namnval == "" || emailval == null || emailval == "" ){
-        alert("Fyll dina uppgifter korrekt");
+    if(namnval == null || namnval == "" || emailval == null || emailval == "" || numberval == null || numberval == "" || meddelandeval == null || meddelandeval == "" ){
+        alert("Du måste fylla i alla fälten");
         return false;
     }
+    document.forms["form"].submit();
+    document.forms["form"].reset();
+    alert("Ditt meddelande har nu skickats");
     return true;
 
 }
