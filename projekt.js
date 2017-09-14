ffb16d12d357ffc7b0f11932be64728e6cf73e59
areimertz <amanda@reimertz.se>
2017-09-14 14:18:01 +0200

    hh

--- projekt.js
+++ projekt.js
@@ -1,38 +1,29 @@
 
 var usernameElement = document.getElementById("username");
 var numberElement = document.getElementById("number");
-
-usernameElement.addEventListener("keyup", function(){
-    var value = usernameElement.value;
-    var usernameregex = /([a-zA-Z]){3,}/
-    var isValid = usernameregex.test(value);
-    console.log(usernameregex.test(value));
-
-    usernameElement.style.backgroundColor = isValid ? "green" : "red";
-})
-
-var numberElement = document.getElementById("number");
-
-numberElement.addEventListener("keyup", function(){
-    var value = numberElement.value;
-    var numberregex = /([0-9]){10,}/
-    var isValid = numberregex.test(value);
-    console.log(numberregex.test(value));
-
-    numberElement.style.backgroundColor = isValid ? "green" : "red";
-})
-
-
 var emailElement = document.getElementById("email");
 
-emailElement.addEventListener("keyup", function(){
-    var value = emailElement.value;
-    var emailregex = /[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}/
-    var isValid = emailregex.test(value);
-    console.log(emailregex.test(value));
+function validate(element, regex) {
+    var value = element.value;
 
-    emailElement.style.backgroundColor = isValid ? "green" : "red";
-})
+    var isValid = regex.test(value);
+
+    element.style.backgroundColor = isValid ? "green" : "red";
+}
+
+usernameElement.addEventListener("keyup", function(){
+    validate(usernameElement, /([a-zA-Z]){3,}/)
+});
+
+numberElement.addEventListener("keyup", function () {
+    validate(numberElement, /([0-9]){10,}/)
+});
+
+emailElement.addEventListener("keyup", function () {
+    validate(emailElement, /[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}/ )
+});
+
+
 
 var meddelandeElement = document.getElementById("meddelande");
 
